65388dbb739742234c3ece4588c51449ef18097f
rany0318 <82741647+rany0318@users.noreply.github.com>
2021-04-25 17:03:25 +0800

    player打完結束

---
+++ 2D_Rockman/Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動速度"),Range(0, 1000)]
+    public float speed = 10.5f;
+    [Header("跳要高度"),Range(0, 3000)]
+    public int jump = 100;
+    [Range(0, 200)]
+    public float hp = 100;
+    [Header("是否在地板上"), Tooltip("儲存角色是否在地板上")]
+    public bool isGrounded;
+    [Header("子彈"), Tooltip("角色要發射的子彈物件")]
+    public GameObject bullet;
+    [Header("子彈生成點"), Tooltip("生成子彈的位置")]
+    public Transform bulletPoint;
+    [Range(0, 5000)]
+    [Header("開槍音效"), Tooltip("開槍的聲音")]
+    public AudioClip bulletSound;
+
+    private AudioSource aud;
+    private Rigidbody2D rig;
+    private Animator ani;
+
+}
